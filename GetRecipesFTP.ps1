3478a67601d30a4e78c4498186c2c12d3b27cec0
Igor Kuznetsov <ik1@bk.ru>
2022-05-16 17:55:55 +0700

    add france

--- GetRecipesFTP.ps1
+++ GetRecipesFTP.ps1
@@ -57,6 +57,9 @@ DownloadFtpDirectory $url $credentials1 $targetdir"Piter FU1"
 $url = "ftp://10.10.21.31/PlcLogic/json/"
 DownloadFtpDirectory $url $credentials1 $targetdir"Piter FU2"
 
+$url = "ftp://10.10.15.31/PlcLogic/json/"
+DownloadFtpDirectory $url $credentials1 $targetdir"FRANCE"
+
 
 if(!(Test-Path -Path $archivepath)){
     New-Item -ItemType directory -Path  $archivepath
